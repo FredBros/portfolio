5393f13c4c4eb5b1c89249199cd5600c77ad380c
Fred Brossard <fred.b.devweb@gmail.com>
2023-02-13 09:59:58 +0100

    Feat : footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      Designed & Built by Fred Brossard
+      <style jsx>{`
+        footer {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding-bottom : 10px;
+        }
+      `}</style>
+    </footer>
+  );
 }
 
 export default Footer
